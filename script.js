3fcd3ae01c268ddf59999df8c59fd0db9e428a2f
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-05-16 18:42:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your code here
+  const divs = document.querySelectorAll('.image');
+
+    let draggedItem = null;
+
+    divs.forEach(div => {
+      div.addEventListener('dragstart', () => {
+        draggedItem = div;
+        div.classList.add('selected');
+      });
+
+      div.addEventListener('dragend', () => {
+        draggedItem = null;
+        div.classList.remove('selected');
+      });
+
+      div.addEventListener('dragover', e => {
+        e.preventDefault();
+      });
+
+      div.addEventListener('dragenter', e => {
+        e.preventDefault();
+        div.classList.add('selected');
+      });
+
+      div.addEventListener('dragleave', () => {
+        div.classList.remove('selected');
+      });
+
+      div.addEventListener('drop', () => {
+        if (draggedItem && draggedItem !== div) {
+          const tempHTML = div.innerHTML;
+          const tempClass = div.className;
+          div.innerHTML = draggedItem.innerHTML;
+          div.className = draggedItem.className;
+          draggedItem.innerHTML = tempHTML;
+          draggedItem.className = tempClass;
+        }
+        div.classList.remove('selected');
+      });
+    });
+ 
